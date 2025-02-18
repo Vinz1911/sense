e3d98df3d3611d32e88129c74f7ce10e8ed3fd27
Vinzenz Weist <Vinz1911@gmail.com>
2025-02-18 01:03:36 +0100

    [M] added license info

--- src/dualsense.cpp
+++ src/dualsense.cpp
@@ -1,3 +1,27 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2025 Vinzenz Weist
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 #include <fcntl.h>
 #include <unistd.h>
 #include <filesystem>
@@ -87,7 +111,7 @@ namespace sense {
         thread_pool_[1] = std::thread([this] {
             while (!is_terminated_.load(std::memory_order::relaxed)) {
                 if (const ssize_t bytes = read(io_event_path_.load(std::memory_order::relaxed), &io_event_, sizeof(io_event_)); bytes == sizeof(io_event_)) {
-                    if (io_event_.type != EV_MSC && io_event_.code != MSC_TIMESTAMP) { continue; }
+                    if (io_event_.type != EV_MSC or io_event_.code != MSC_TIMESTAMP) { continue; }
                     current_time_.store(std::chrono::high_resolution_clock::now(), std::memory_order::relaxed);
                 }
             }
